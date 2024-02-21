efdcde0bb6499bb52e1cd355aa1b91943ab75ac4
ajitkumarjena11 <158299220+ajitkumarjena11@users.noreply.github.com>
2024-02-21 16:02:18 +0530

    commited

---
+++ Datatype/src/p1/A.java
@@ -0,0 +1,18 @@
+package p1;
+//Rules to create variable in java
+public class A {
+	static int id;
+	static int employeeId;
+	static int yourEmployeeId;
+	static int emp_id;
+	static int $id;
+	static int emp2;
+//	static int emp id;//error
+//	static int 2emp;//error
+//	static int new;//error
+//	static int static ;//error
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ Datatype/src/p1/B.java
@@ -0,0 +1,16 @@
+package p1;
+//method naming conventions....
+public class B {
+	public static void main(String[] args) {
+		System.out.println();	
+	}
+	public void employee() {
+		
+	}
+	public void employeeId() {
+		
+	}
+	public void yourEmployeeId() {
+		
+	}
+}
